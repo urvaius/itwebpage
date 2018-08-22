f63ae2af0d437425d196cb6f1fa2148eb4cc2ff7
urvaius <urvaius@gmail.com>
2018-08-22 10:23:20 -0700

    jquery working in typescript file finally.

--- src/ts/typecode.ts
+++ src/ts/typecode.ts
@@ -4,8 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
 import 'jquery';
 import 'bootstrap/dist/js/bootstrap';
-// import $ from 'jquery';
-
+import * as $ from 'jquery';
 // import Icon from '../image/images.jpg';
 // import * as codeblock from './code';
 
@@ -14,5 +13,19 @@ if (process.env.NODE_ENV !== 'production') {
   console.log('We Are in Development mode!!');
 }
 
-
+function changeColor(color: string) {
+  
+  document.getElementById('purps').style.color = color;
+  document.getElementById('purps').textContent = "I am now Red";
+  // testing jquery does work here
+}
 console.log('this is typescript file yay');
+function testLog(){
+  console.log('check typescriptfunction')
+}
+
+$( "#btn1" ).click(function() {
+  changeColor('red');
+});
+
+export {changeColor, testLog};
