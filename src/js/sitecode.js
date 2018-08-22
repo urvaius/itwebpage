412fcce3c8c8ed545fab75c4d01f192abaf0e2d2
urvaius <urvaius@gmail.com>
2018-08-22 09:08:30 -0700

    chaning for ezra to use this as well

--- src/js/sitecode.js
+++ src/js/sitecode.js
@@ -1,14 +1,15 @@
 // import _ from 'lodash';
-import '../css/style.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'font-awesome/css/font-awesome.min.css';
-import 'jquery';
-import 'bootstrap/dist/js/bootstrap';
+// import '../css/style.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'font-awesome/css/font-awesome.min.css';
+// import 'jquery';
+// import 'bootstrap/dist/js/bootstrap';
+
 // import $ from 'jquery';
 
 // import Icon from '../image/images.jpg';
 // import * as codeblock from './code';
 
-if (process.env.NODE_ENV !== 'production') {
-  console.log('We Are in Development mode!!');
-}
+// if (process.env.NODE_ENV !== 'production') {
+//  console.log('We Are in Development mode!!');
+// }
--- src/ts/typecode.ts
+++ src/ts/typecode.ts
@@ -1,1 +1,18 @@
+// import _ from 'lodash';
+import '../css/style.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+import 'jquery';
+import 'bootstrap/dist/js/bootstrap';
+// import $ from 'jquery';
+
+// import Icon from '../image/images.jpg';
+// import * as codeblock from './code';
+
+
+if (process.env.NODE_ENV !== 'production') {
+  console.log('We Are in Development mode!!');
+}
+
+
 console.log('this is typescript file yay');
